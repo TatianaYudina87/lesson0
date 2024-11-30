2f7cbeb84d7dac40b04fde6f8a630a5f924115ee
TatianaYudina87 <tanyamaz8@gmail.com>
2024-11-30 04:25:28 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,20 @@
+#1st program
+print((9 ** 0.5) * 5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2 * 2 + 2)
+print(2 * (2+2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+#4th program
+print ('123.456')
+print(123.456 * 10)
+print(1234.56 % 10)
+print(4.56 // 1)
+print(int (4.0))
+
+#Работа над ошибками #4th program
+print(int(((float('123.456')) * 10) % 10))
